fa17c4f29b076720d6930121a30d16d2b2a966ea
HLHotBlooded <40052526+HLHotBlooded@users.noreply.github.com>
2018-08-15 09:44:03 +0800

    Initial commit

---
+++ src/com/lin/yc/yc.java
@@ -0,0 +1,13 @@
+package com.lin.yc;
+
+/**
+ * @author zero
+ * @program: workspace1
+ * @Date 2018/8/14-14:22
+ * @Description:
+ **/
+public class yc {
+ public static void main(String[] args) {
+  System.out.println("hello,YC!");
+ }
+}
